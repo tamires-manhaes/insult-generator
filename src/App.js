8f98d9e931314c377dc29e122f124540d3811bfb
Tamires Manhães <tamiresmanhaes4@gmail.com>
2020-11-26 16:21:16 -0300

    feat: all components and logic working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, lazy } from 'react';
+import axios from 'axios';
+import config from './api';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Insult Generator</h1>
+    </header>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span>
+        Feito com 💙 por <br/>
+        <a href="https://tamires-manhaes.github.io" rel="noreferrer" target="_blank">Tamires Manhães</a>
+      </span>
+    </footer>
+  )
+}
+
+const Container = ({ children }) => {
+  return (
+    <div className="container">
+        <Header />
+        <div className="wrapper">
+          { children }
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+const Button = ({ title, action }) => {
+  return(
+    <button onClick={action} className="buttonAction">
+      <span>{title}</span>
+    </button>
+  )
+}
+
+const InsultCard = ({ insult }) => {
+  return (
+    <div className="insultCard">
+      <span>{ insult }</span>
+    </div>
+  )
+}
+
+const Loading = () => {
+  return (
+    <div className="loader"></div>
+  )
+}
 
 function App() {
+  const [insult, setInsult] = useState();
+  const [loading, setLoading] = useState(false);
+
+  async function action(){
+    setLoading(true);
+    await axios.get(config.baseUrl, {
+      headers: config.headers
+    }).
+    then(response => { setLoading(true); setInsult(response.data);});
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {loading === true && insult !== null ? (
+        <Loading />
+      ) : ( 
+        <></>
+      )}
+
+      {insult !== null && insult !== undefined ? (
+        <>
+          <InsultCard insult={insult.insult}/>
+          <Button title="Click again" action={action}/>
+        </>
+      ): (<><Button title="Click to generate insult" action={action}/></>)}
+        
+    </Container>
   );
 }
 
